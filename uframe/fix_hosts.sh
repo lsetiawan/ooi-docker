081a073e2fe5b230a81942d97eae6006d13c6900
Peter Cable <petercable@gmail.com>
2017-02-02 14:48:15 -0800

    UFrame docker updates

--- uframe/fix_hosts.sh
+++ uframe/fix_hosts.sh
@@ -1,6 +1,13 @@
-sed -i 's/localhost/qpid/g' edex/data/utility/edex_static/base/ooi/config/alertAlarm.json
-sed -i 's/localhost/qpid/g' edex/etc/*.sh
-sed -i 's/DB_ADDR=localhost/DB_ADDR=postgres/g' edex/bin/setup.env
-sed -i 's/BROKER_ADDR=localhost/BROKER_ADDR=qpid/g' edex/bin/setup.env
-sed -i 's/contactpoints=127.0.0.1/contactpoints=cassandra/g' edex/data/utility/edex_static/base/ooi/ooi_configure.properties
-sed -i 's;ooi.stream_engine.url=http://localhost:5000;ooi.stream_engine.url=http://stream_engine:5000;g' edex/data/utility/edex_static/base/ooi/ooi_configure.properties
+export AA=edex/data/utility/edex_static/base/ooi/config/alertAlarm.json
+export ETC=edex/etc/*.sh
+export SETUP=edex/bin/setup.env
+export BASE_PROP=edex/data/utility/edex_static/base/ooi/ooi_configure.properties
+
+sed -i 's/localhost/qpid/g' $AA
+sed -i 's/localhost/qpid/g' $ETC
+sed -i 's/DB_ADDR=localhost/DB_ADDR=postgres/g' $SETUP
+sed -i 's/BROKER_ADDR=localhost/BROKER_ADDR=qpid/g' $SETUP
+sed -i 's/INIT_MEM=512/INIT_MEM=1536/g' edex/etc/ooi.sh
+
+sed -i 's/contactpoints=127.0.0.1/contactpoints=cassandra/g' $BASE_PROP
+sed -i 's;ooi.stream_engine.url=http://localhost:5000;ooi.stream_engine.url=http://stream_engine:5000;g' $BASE_PROP
