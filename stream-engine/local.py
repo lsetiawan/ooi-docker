84db44ddc4eb5b067b2c5534c7fc514b87c56dc2
Peter Cable <petercable@gmail.com>
2017-02-02 10:43:40 -0800

    Stream engine update

---
+++ stream-engine/local.py
@@ -0,0 +1,5 @@
+CASSANDRA_CONTACT_POINTS = ["cassandra"]
+ANNOTATION_HOST = "uframe"
+ASSET_HOST = "uframe"
+STREAM_METADATA_SERVICE_URL = "http://uframe:12571/streamMetadata"
+PARTITION_METADATA_SERVICE_URL = "http://uframe:12571/partitionMetadata"
